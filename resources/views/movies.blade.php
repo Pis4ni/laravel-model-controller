4505cae23be7059d723e64a580605a204de6cd61
Pis4ni <pis4wrk@gmail.com>
2023-10-13 16:01:36 +0200

    add: re-routing from home page (not anymore used)

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-bs-theme="dark">
 
 <head>
   <meta charset="utf-8">
@@ -23,11 +23,27 @@
         @foreach ($movies as $movie)
         <div class="col-3">
           <div class="card">
+            
             <div class="card-body">
               <div class="card-header">
                 <div class="card-title">
-                  {{$movie['title']}}
+                  <h2>
+                    {{$movie['title']}}
+                  </h2>
                 </div>
+                <div class="card-subtitle">
+                  <h4 class="text-muted">
+                    {{$movie['original_title']}}
+                  </h4>
+                </div>
+              </div>
+              <div class="card-content">
+                {{$movie['nationality']}}
+                <br>
+                {{$movie['date']}}
+              </div>
+              <div class="card-footer">
+                {{$movie['vote']}}
               </div>
             </div>
           </div>
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
+Route::get('/', [PageController::class, 'movies'])->name('movies');
 Route::get('/movies', [PageController::class, 'movies'])->name('movies');
